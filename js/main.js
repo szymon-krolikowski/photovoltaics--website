9352fd0aac9cec20e060a32a803776e54082fc29
Szymon Królikowski <krolikowski.szymon@wp.pl>
2021-02-17 12:29:40 +0100

    ADD script to question section

--- js/main.js
+++ js/main.js
@@ -24,27 +24,60 @@
     let spanAnnualBillWithInstallation = document.querySelector(".js-annualBillWithInstallation");
     let spanYouSave = document.querySelector(".js-youSave");
 
-    const getValueRangeInput = () => {
-        inputRange.oninput = () => {
-            let valueRangeInput = inputRange.value;
-            let annualProduction = ((valueRangeInput / 0.61) * 12).toFixed(0);
-            let installationSize = ((annualProduction / 1000) * 1.3).toFixed(2);
-            let installationCost = (installationSize * 3000).toFixed(0);
-            let installationCostWithDotation = installationCost - 5000;
-            let annualBill = valueRangeInput * 12;
-            let annualBillWithInstallation = (valueRangeInput * 0.52).toFixed(2);
-            let youSave = (annualBill * 25) - installationCost - (annualBillWithInstallation * 25);
+    inputRange.addEventListener("input", () => {
+        let valueRangeInput = inputRange.value;
+        let annualProduction = ((valueRangeInput / 0.61) * 12).toFixed(0);
+        let installationSize = ((annualProduction / 1000) * 1.3).toFixed(2);
+        let installationCost = (installationSize * 3000).toFixed(0);
+        let installationCostWithDotation = installationCost - 5000;
+        let annualBill = valueRangeInput * 12;
+        let annualBillWithInstallation = (valueRangeInput * 0.52).toFixed(2);
+        let youSave = (annualBill * 25) - installationCost - (annualBillWithInstallation * 25);
 
-            spanInstallationSize.innerText = installationSize + " kWp";
-            spanAnnualProduction.innerText = annualProduction + " kWh";
-            spanInstallationCost.innerText = installationCost + " zł";
-            spanInstallationCostWithDotation.innerText = installationCostWithDotation + " zł";
-            spanAnnualBill.innerText = annualBill + " zł";
-            spanAnnualBillWithInstallation.innerText = annualBillWithInstallation + " zł";
-            spanYouSave.innerText = youSave + " zł";
-        };
-    };
-
-    getValueRangeInput();
+        spanInstallationSize.innerText = installationSize + " kWp";
+        spanAnnualProduction.innerText = annualProduction + " kWh";
+        spanInstallationCost.innerText = installationCost + " zł";
+        spanInstallationCostWithDotation.innerText = installationCostWithDotation + " zł";
+        spanAnnualBill.innerText = annualBill + " zł";
+        spanAnnualBillWithInstallation.innerText = annualBillWithInstallation + " zł";
+        spanYouSave.innerText = youSave + " zł";
+    });
 }
 
+{
+    let askParagraphs = document.querySelectorAll(".question__askParagraph");
+    let answerParagraphs = document.querySelectorAll(".question__answerParagraph");
+
+    askParagraphs.forEach(askParagraph => {
+        askParagraph.addEventListener("click", () => {
+            answerParagraphs.forEach(answerParagraph => {
+                answerParagraph.style = "dispaly: block";
+            })
+        })
+    })
+}
+
+
+{
+    const accordionItemHeaders = document.querySelectorAll(".question__itemHeader");
+
+    accordionItemHeaders.forEach(accordionItemHeader => {
+        accordionItemHeader.addEventListener("click", () => {
+
+        const currentlyActiveAccordionItemHeader = document.querySelector(".question__itemHeader.active");
+        if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!==accordionItemHeader) {
+            currentlyActiveAccordionItemHeader.classList.toggle("active");
+            currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
+        };
+
+        accordionItemHeader.classList.toggle("active");
+
+        const accordionItemBody = accordionItemHeader.nextElementSibling;
+
+        if(accordionItemHeader.classList.contains("active")) {
+        accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+        }
+        else { accordionItemBody.style.maxHeight = 0};
+        });
+    });
+}
